7c7ce1c604e3593c0cda1cbd5ff3d35e5c8c7735
Maximilian90 <105641605+Maximilian90@users.noreply.github.com>
2023-05-10 12:32:55 +0200

    1

--- src/components/pages/EventDetaljer.vue
+++ src/components/pages/EventDetaljer.vue
@@ -3,10 +3,11 @@ import RightSidebar from '../RightSidebar.vue';
 </script>
 
 <template>
-    <section class="darkframe">
+    <section class="event_layout">
         <div class="header">
             <img class="banner" src="../../assets/bannerheader1.png" alt="">
         </div>
+        <div class="darkframe"></div>
         <div class="event_dlayout">
             <div class="event_content">
                 <div class="edtitelbg">
@@ -14,17 +15,27 @@ import RightSidebar from '../RightSidebar.vue';
                     <p class="eddato">Lørdag d. 13 Maj - 16:00-18:00</p>
                 </div>
                 <img class="edbanner" src="../../assets/skate60001.jpg" alt="">
+                
+                <div class="edbox">
+                    <h4 class="edh4">Om Eventet</h4>
+                    <p>Vi i skate6000 har fået bygget nogle bænke som skal stå i Nicolai gården over sommeren 2023. Vi afholder indvielse af de nye bænke lørdag d. 13 maj kl 16:00. Der vil være musik og god session. </p>
+                    <h4 class="edh4">Arrangør</h4>
+                    <p>Skate6000 & Batiste Co</p>
+                    <h4 class="edh4">Adresse</h4>
+                    <p>Skolegade 2 C, 6000 Kolding</p>
+                    <h4 class="edh4">Pris</h4>
+                    <p>Gratis</p>
+                </div>
+
+                
             </div>
-            <div class="edbox">
-                <h4 class="edh4">Om Eventet</h4>
-                <p>Vi i skate6000 har fået bygget nogle bænke som skal stå i Nicolai gården over sommeren 2023. Vi afholder indvielse af de nye bænke lørdag d. 13 maj kl 16:00. Der vil være musik og god session. </p>
-                <h4 class="edh4">Arrangør</h4>
-                <p>Skate6000 & Batiste Co</p>
-                <h4 class="edh4">Adresse</h4>
-                <p>Skolegade 2 C, 6000 Kolding</p>
-                <h4 class="edh4">Pris</h4>
-                <p>Gratis</p>
+
+            <div class="eventd_sidebar">
+                <RightSidebar/>
             </div>
+
+            
+
         </div>
         <router-link class="edlink" to="/Event"><button class="edknap" id="edpil">Tilbage</button></router-link>
     </section>
