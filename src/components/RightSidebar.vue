bf6c7e18fd6b4997a50a735937b4af25300cf838
Maximilian90 <105641605+Maximilian90@users.noreply.github.com>
2023-05-10 10:03:45 +0200

    11

--- src/components/RightSidebar.vue
+++ src/components/RightSidebar.vue
@@ -1,30 +1,86 @@
 <script setup>
+import { computed, ref, onMounted } from 'vue';
 
+const events = ref([]);
+
+const getPlainDate = (dateString) => {
+  if (!dateString) {
+    return '';
+  }
+  const year = dateString.substring(0, 4);
+  const month = dateString.substring(4, 6);
+  const day = dateString.substring(6, 8);
+
+  return `${year}-${month}-${day}`;
+};
+
+const getPlainText = (content) => {
+  const parser = new DOMParser();
+  const html = parser.parseFromString(content, 'text/html');
+  const firstParagraph = html.querySelector('p');
+  const plainText = firstParagraph.textContent;
+  const noHtmlText = plainText.replace(/<(?:.|\n)*?>/gm, '');
+  return noHtmlText;
+};
+
+const getFeaturedImageUrl = (event) => {
+  const content = event.content.rendered;
+  const match = content.match(/<img[^>]+src="([^">]+)"/);
+  if (match) {
+    return match[1];
+  } else {
+    return '';
+  }
+};
+
+const filteredEvents = computed(() => {
+  const currentMonth = new Date().toISOString().slice(0, 7);
+  const eventsInCurrentMonth = events.value.filter(event => {
+    return event.plainDate.startsWith(currentMonth);
+  });
+  const sortedEvents = eventsInCurrentMonth.sort((a, b) => {
+    const dateA = new Date(a.plainDate);
+    const dateB = new Date(b.plainDate);
+    return dateA - dateB;
+  });
+  return sortedEvents;
+});
+
+const upcomingEvents = computed(() => {
+  const now = new Date();
+  return filteredEvents.value.filter(event => new Date(event.plainDate) > now);
+});
+
+onMounted(() => {
+  fetch('https://sesh.mg-visions.com/index.php/wp-json/wp/v2/event')
+    .then(response => response.json())
+    .then(data => {
+      events.value = data.map(event => {
+        const plainDate = getPlainDate(event.acf.event_date);
+        const plainText = getPlainText(event.content.rendered);
+        return { ...event, plainText, plainDate };
+      });
+    });
+});
 
 </script>
 
 
+
 <template>
-
-
-    
     <div class="sidebar">
-        <h2 class="headtext_sidebar">Seneste tilføjede Events</h2>
-        <hr class="line2">
-
-        <div class="latest_row">
-            <div class="latest_img"> 
-                <img/>
-            </div>
-            <div class="latest_content">
-                <h4 >test title</h4>       
-                <p class="p_latest">test date</p>
-            </div>
+      <h2 class="headtext_sidebar">Kommende Events</h2>
+      <hr class="sidebar_line" />
+      <div v-for="event in upcomingEvents" :key="event.id" class="sidebar_event">
+        <img class="sidebar_img" :src="getFeaturedImageUrl(event)" :alt="event.title.rendered" />
+        <div class="sidebar_event_details">
+          <h3>{{ event.title.rendered }}</h3>
+          <p>{{ new Date(event.plainDate).toLocaleDateString('da-DK', {month: 'long', day: 'numeric' }) }}</p>
         </div>
-
+      </div>
     </div>
-
 </template>
+  
 
 <style scoped>
 
@@ -34,20 +90,6 @@
     flex-wrap: wrap;
 }
 
-.latest_content {
-    flex: 1 1 auto;
-    flex-direction: column;
-    align-items: center;
-    margin-top: 2em;
-    margin-left: 1em;
-    margin-right: 1em;
-}
-.latest_img {
-    width: 80px;
-    height: 80px;
-    background-color: rgb(79, 124, 163);
-    margin-left: 2em;
-    margin-top: 2em;
-}
+
 
 </style>
