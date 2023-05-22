0126e00cffb95fe4c78f1a6edf3fde76c23973e0
Mettelund3 <Melu32113@edu.ucl.dk>
2023-05-22 10:59:25 +0200

    j

--- src/router/index.js
+++ src/router/index.js
@@ -4,39 +4,31 @@ import Event from '../components/pages/Event.vue'
 import EventDetaljer from '../components/pages/EventDetaljer.vue'
 import NotFound from '../components/NotFound.vue'
 
-
-
 const router = createRouter({
     history: createWebHashHistory(import.meta.env.BASE_URL),
     routes: [
-
         {
             path: '/',
             name: 'home',
             component: Home
         },
-        
         {
             path: '/event',
             name: 'event',
             component: Event
         },
-
         {
             path: '/eventdetaljer/:id',
             name: 'eventdetaljer',
             component: EventDetaljer,
             props:true
         },
-
         {
             path: '/:pathMatch(.*)*',
             name: 'notFound',
             component: NotFound
         },
-
     ]
-
 })
 
 export default router
